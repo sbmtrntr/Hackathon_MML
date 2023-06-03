c1ae1ae70222f0ecfd2020480658ef353e195099
Mondsichel <112801340+Mondsichel@users.noreply.github.com>
2023-06-03 19:51:37 +0900

    [update] mml_project

--- mml_project/touchtyperman/train.py
+++ mml_project/touchtyperman/train.py
@@ -10,8 +10,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms
 
-import cnn
-import data_prep
+# import cnn
+# import data_prep
 
 
 
@@ -20,10 +20,10 @@ threshold_loss = 1e-4
 
 
 class Train:
-    def __init__(self, net, train_loader, test_loader):
+    def __init__(self, net, train_loader):
         self.net = net
         self.train_loader = train_loader
-        self.test_loader = test_loader
+        # self.test_loader = test_loader
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -45,7 +45,7 @@ class Train:
             total = 0
             for data in self.train_loader:
                 inputs, labels = data[0].to(self.device), data[1].to(self.device)
-                
+
                 optimizer.zero_grad()
 
                 outputs = self.net(inputs)
@@ -58,15 +58,15 @@ class Train:
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
                 running_loss += loss.item()
-                
+
             train_loss = running_loss / total
             train_acc = 100 * correct / total
             print(f'train\tloss: {train_loss:f}  accuracy: {train_acc:.3f} %')
-            self.test_accuracy()
+            # self.test_accuracy()
 
             epoch += 1
 
-        torch.save(self.net.state_dict(), 'models/params/model_weight.pth')
+        # torch.save(self.net.state_dict(), 'models/params/model_weight.pth')
         print("---------------")
         print('Finished Training')
         print()
@@ -77,30 +77,28 @@ class Train:
 
 
 
-    def test_accuracy(self):
-        running_loss = 0.0
-        correct = 0
-        total = 0
+    # def test_accuracy(self):
+    #     running_loss = 0.0
+    #     correct = 0
+    #     total = 0
 
-        with torch.no_grad():
-            for data in self.test_loader:
-                images, labels = data
-                
-                outputs = self.net(images)
-                
-                loss = self.criterion(outputs, labels)
-                running_loss += loss.item()
+    #     with torch.no_grad():
+    #         for data in self.test_loader:
+    #             images, labels = data
 
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-                
-        test_loss = running_loss / total
-        test_acc = 100 * correct / total
-        
-        print(f'test\tloss: {test_loss:f}  accuracy: {test_acc:.3f} %')
+    #             outputs = self.net(images)
 
+    #             loss = self.criterion(outputs, labels)
+    #             running_loss += loss.item()
 
+    #             _, predicted = torch.max(outputs.data, 1)
+    #             total += labels.size(0)
+    #             correct += (predicted == labels).sum().item()
+
+    #     test_loss = running_loss / total
+    #     test_acc = 100 * correct / total
+
+    #     print(f'test\tloss: {test_loss:f}  accuracy: {test_acc:.3f} %')
 
 
 
@@ -108,25 +106,27 @@ class Train:
 
 
 
-if __name__ == '__main__':
-    transform = transforms.Compose([transforms.ToTensor()])
-
-    train_file_list, test_file_list = data_prep.make_filepath_list()
-
-    train_dataset = data_prep.EyeDataset(file_list=train_file_list, transform=transform)
-    test_dataset = data_prep.EyeDataset(file_list=test_file_list, transform=transform)
-
-    batch_size = 4
-
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=False)
 
 
-    net = cnn.CNN()
+# if __name__ == '__main__':
+#     transform = transforms.Compose([transforms.ToTensor()])
 
-    max_epoch = 50
-    train = Train(net, train_loader, test_loader)
-    train.training(max_epoch)
+#     train_file_list, test_file_list = data_prep.make_filepath_list()
+
+#     train_dataset = data_prep.EyeDataset(file_list=train_file_list, transform=transform)
+#     test_dataset = data_prep.EyeDataset(file_list=test_file_list, transform=transform)
+
+#     batch_size = 4
+
+#     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+#     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=False)
+
+
+#     net = cnn.CNN()
+
+#     max_epoch = 50
+#     train = Train(net, train_loader, test_loader)
+#     train.training(max_epoch)
 
 
 
--- mml_project/touchtyperman/views.py
+++ mml_project/touchtyperman/views.py
@@ -4,37 +4,42 @@ from django.http import JsonResponse
 
 
 
-# import numpy as np
-# from PIL import Image
-# import time
-# import glob
+import numpy as np
+from PIL import Image
+import time
+import glob
+import os
+import shutil
 
-# import dlib
-# from imutils import face_utils
-# import cv2
-# import time
+import dlib
+from imutils import face_utils
+import cv2
+import time
 
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from torchvision import transforms
-
-
-# import cnn
-
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
 
 
+import touchtyperman.cnn
+import touchtyperman.data_prep
+import touchtyperman.train
 
 
-# face_detector = dlib.get_frontal_face_detector()
-# predictor_path = 'touchtyperman/models/shape_predictor_68_face_landmarks.dat'
-# face_predictor = dlib.shape_predictor(predictor_path)
 
 
-# transform = transforms.Compose([transforms.ToTensor()])
 
-# net = cnn.CNN()
-# net.load_state_dict(torch.load('ouchtyperman/models/params/model_weight.pth'))
+face_detector = dlib.get_frontal_face_detector()
+predictor_path = 'touchtyperman/models/shape_predictor_68_face_landmarks.dat'
+face_predictor = dlib.shape_predictor(predictor_path)
+
+cnn_dict = {}
+transform = transforms.Compose([transforms.ToTensor()])
+
+softmax = nn.Softmax(dim=1)
+
+cnt_dict = {}
 
 
 
@@ -55,149 +60,90 @@ def index(request):
 
 
 
-
-# def detect_landmark(img):
-#     faces = face_detector(img, 1)
-#     if len(faces) == 0:
-#         return None
-#     else:
-#         landmark = face_predictor(img, faces[0])
-#         landmark = face_utils.shape_to_np(landmark)
-#         return landmark
-
-
-
-
-
-# def eye_crop(img, landmark):
-#     left_x = landmark[0][0] - 3
-#     right_x = landmark[16][0] + 3
-#     top_y = max([landmark[19][1], landmark[24][1]]) - 3
-#     bottom_y = landmark[8][1] + 3
-
-#     face_region = {'left_x': left_x, 'right_x': right_x,'top_y': top_y, 'bottom_y': bottom_y}
-
-
-#     # right_eye_region = {'left_x': landmark[36][0] - 5, 'right_x': landmark[39][0] + 3,
-#     #                     'top_y': landmark[37][1] - 3 if landmark[37][1] < landmark[38][1] else landmark[38][1] - 3,
-#     #                     'bottom_y': landmark[41][1] + 3 if landmark[41][1] > landmark[40][1] else landmark[40][1] + 3}
-
-#     # left_eye_region = {'left_x': landmark[42][0] - 3, 'right_x': landmark[45][0] + 5,
-#     #                    'top_y': landmark[43][1] - 3 if landmark[43][1] < landmark[45][1] else landmark[45][1] - 3,
-#     #                    'bottom_y': landmark[47][1] + 3 if landmark[47][1] > landmark[46][1] else landmark[46][1] + 3}
-
-#     eye_region = {'left_x': landmark[36][0] - 5, 'right_x': landmark[45][0] + 5,
-#                   'top_y': max(landmark[19][1], landmark[24][1]) - 3,
-#                 #   'top_y': max(landmark[37][1], landmark[38][1], landmark[43][1], landmark[44][1]) - 3,
-#                   'bottom_y': max(landmark[40][1], landmark[41][1], landmark[46][1], landmark[47][1]) + 3}
-
-
-#     # right_cropped_img = img[right_eye_region['top_y']:right_eye_region['bottom_y'], right_eye_region['left_x']:right_eye_region['right_x']]
-#     # left_cropped_img = img[left_eye_region['top_y']:left_eye_region['bottom_y'], left_eye_region['left_x']:left_eye_region['right_x']]
-
-#     cropped_img = img[eye_region['top_y']:eye_region['bottom_y'], eye_region['left_x']:eye_region['right_x']]
-#     print(cropped_img.shape)
-
-#     return cropped_img, eye_region, face_region
-#     # return right_cropped_img, left_cropped_img, right_eye_region, left_eye_region, face_region
-
-
-
-
-# def draw(landmark, img):
-#     face_outline = list(landmark[:17])
-#     face_outline.extend(reversed(list(landmark[17:27])))
-
-#     new_img = np.full((1020, 720), 255, np.uint8)
-#     for i in range(len(face_outline)):
-#         if i == len(face_outline) - 1:
-#             cv2.line(new_img, pt1=face_outline[i], pt2=face_outline[0], color=(0, 0, 0), thickness=2)
-#         else:
-#             cv2.line(new_img, pt1=face_outline[i], pt2=face_outline[i+1], color=(0, 0, 0), thickness=2)
-
-#     # right_eye_img, left_eye_img, right_eye_region, left_eye_region, face_region = eye_crop(img, landmark)
-#     eye_img, eye_region, face_region = eye_crop(img, landmark)
-#     # new_img[right_eye_region['top_y']:right_eye_region['bottom_y'], right_eye_region['left_x']:right_eye_region['right_x']] = right_eye_img
-#     # new_img[left_eye_region['top_y']:left_eye_region['bottom_y'], left_eye_region['left_x']:left_eye_region['right_x']] = left_eye_img
-#     print(new_img[eye_region['top_y']:eye_region['bottom_y'], eye_region['left_x']:eye_region['right_x']].shape)
-#     new_img[eye_region['top_y']:eye_region['bottom_y'], eye_region['left_x']:eye_region['right_x']] = eye_img
-
-#     face_img = new_img[face_region['top_y']:face_region['bottom_y'], face_region['left_x']:face_region['right_x']]
-
-#     # return transform(Image.fromarray(face_img))
-
-
-
-
-
-
-
-# def test(file_list, eye_img0):
-#     cos = nn.CosineSimilarity(dim=0)
-#     for i, f in enumerate(file_list):
-#         img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
-#         landmark = detect_landmark(img)
-#         if landmark is not None:
-#             # eye_img = eye_crop(img, landmark, transform)
-#             eye_img = draw(landmark, img)
-#             result = cos(torch.flatten(eye_img0), torch.flatten(eye_img)).item()
-#             print(f'{result:.6f}\t{i}\t{result < 0.96}\t{f}')
-
-
-
-
-
-
-
-
-
-
-cnt_0 = 200
-cnt_1 = 200
+# cnt_0 = 0
+# cnt_1 = 0
+# cnt_2 = 0
 
 
 # @csrf_exempt
 def get_data(request):
-    global cnt_0, cnt_1
+    global cnn_dict, cnt_dict
     dtype = request.POST.get('type')
+    user = request.POST.get('user')
     file = request.FILES['img']
 
 
     if dtype == 'temp':
-        file_name = 'touchtyperman/img/temp/image.jpg'
+        net = cnn_dict[user]
+        file_name = f'touchtyperman/img/{user}/temp/image.jpg'
         with open(file_name, 'wb+') as f:
             for chunk in file.chunks():
                 f.write(chunk)
-        # img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
-        # landmark = detect_landmark(img)
-        # if landmark is None:
-        #     d = {'result': 2}
-        # else:
-        #     test_img0 = transform(Image.open('touchtyperman/img/faces/image0.jpg').convert('L'))
-        #     test_img1 = transform(Image.open('touchtyperman/img/faces/image1.jpg').convert('L'))
-        #     # test_img2 = transform(Image.open('touchtyperman/img/faces/image2.jpg').convert('L'))
-        #     face_img = draw(landmark, img)
-        #     cos0 = cos(torch.flatten(face_img), torch.flatten(test_img0)).item()
-        #     cos1 = cos(torch.flatten(face_img), torch.flatten(test_img1)).item()
-        #     # cos2 = cos(torch.flatten(face_img), torch.flatten(test_img2)).item()
-        #     if cos0 > cos1 + 0.003:
-        #         d = {'result': 0, 'cos0': cos0, 'cos1': cos1}
-        #     else:
-        #         d = {'result': 1, 'cos0': cos0, 'cos1': cos1}
-        d = {'success': True}
+        img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+        landmark = data_prep.detect_landmark(img)
+        if landmark is None:
+            d = {'result': 2}
+        else:
+            eye_img = data_prep.eye_crop(img, landmark)
+            transformed_img = transform(eye_img)
+            output = softmax(net(transformed_img))
+            d = {'success': output}
+        os.remove(file_name)
         return JsonResponse(d)
 
-    else:
-        if dtype == 'face-0':
-            file_name = f'touchtyperman/img/train/0/image_{cnt_0:05}.jpg'
-            cnt_0 += 1
+
+    elif dtype == 'finetuning':
+        if user not in cnn_dict:
+            d = {'success': False}
+            return JsonResponse(d)
         else:
-            file_name = f'touchtyperman/img/train/1/image_{cnt_1:05}.jpg'
-            cnt_1 += 1
+            net = cnn_dict[user]
+
+        train_file_list = data_prep.make_filepath_list(user)
+
+        train_dataset = data_prep.EyeDataset(file_list=train_file_list, transform=transform)
+        # test_dataset = data_prep.EyeDataset(file_list=test_file_list, transform=transform)
+
+        batch_size = 4
+
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+        # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=False)
+
+        max_epoch = 50
+        finetune = train.Train(net, train_loader)
+        finetune.training(max_epoch)
+
+        d = {'success': True}
+        shutil.rmtree(f'touchtyperman/img/{user}/')
+        os.makedirs(f'touchtyperman/img/{user}/temp/', exist_ok=True)
+        os.makedirs(f'touchtyperman/img/{user}/train/0/', exist_ok=True)
+        os.makedirs(f'touchtyperman/img/{user}/train/1/', exist_ok=True)
+        os.makedirs(f'touchtyperman/img/{user}/train/2/', exist_ok=True)
+        return JsonResponse(d)
+
+
+    else:
+        if user not in cnn_dict:
+            cnn_dict[user] = cnn.CNN()
+        if user not in cnt_dict:
+            cnt_dict[user] = [0, 0, 0]
+        if not os.path.isdir(f'touchtyperman/img/{user}/'):
+            os.makedirs(f'touchtyperman/img/{user}/temp/', exist_ok=True)
+            os.makedirs(f'touchtyperman/img/{user}/train/0/', exist_ok=True)
+            os.makedirs(f'touchtyperman/img/{user}/train/1/', exist_ok=True)
+            os.makedirs(f'touchtyperman/img/{user}/train/2/', exist_ok=True)
+        if dtype == 'train-0':
+            file_name = f'touchtyperman/img/{user}/train/0/image_{cnt_dict[user][0]:05}.jpg'
+            cnt_dict[user][0] += 1
+        elif dtype == 'train-1':
+            file_name = f'touchtyperman/img/{user}/train/1/image_{cnt_dict[user][1]:05}.jpg'
+            cnt_dict[user][1] += 1
+        else:
+            file_name = f'touchtyperman/img/{user}/train/2/image_{cnt_dict[user][2]:05}.jpg'
+            cnt_dict[user][2] += 1
         with open(file_name, 'wb+') as f:
             for chunk in file.chunks():
                 f.write(chunk)
         d = {'success': True}
-        # print(cnt)
         return JsonResponse(d)
 
